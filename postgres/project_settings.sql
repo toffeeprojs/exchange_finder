6ccb146c9614cc3f183765a2ade05213cea1a4ea
Toffee35 <nailzagru@gmail.com>
2025-07-11 22:21:21 +0300

    Написаны некоторый обаботчики и изменены конфиги

--- postgres/project_settings.sql
+++ postgres/project_settings.sql
@@ -1,24 +1,80 @@
-CREATE TABLE currencies (
-    name VARCHAR(10) PRIMARY KEY,
-    rate_to_usd NUMERIC(15, 6) NOT NULL CHECK (rate_to_usd > 0)
+CREATE TYPE user_role AS ENUM (
+    'MEM',
+    'MOD',
+    'ADM'
 );
 
 CREATE TABLE users (
     telegram_id BIGINT PRIMARY KEY,
-    location GEOGRAPHY(Point, 4326) NOT NULL
+    location GEOGRAPHY(Point, 4326) NOT NULL,
+
+    telegram_first_name VARCHAR(64) NOT NULL,
+    display_name VARCHAR(64) NULL
+        DEFAULT NULL,
+    telegram_username VARCHAR(32) NULL
+        DEFAULT NULL,
+    telegram_last_name VARCHAR(64) NULL
+        DEFAULT NULL,
+
+    role user_role NOT NULL
+        DEFAULT 'MEM',
+
+    is_disabled BOOLEAN NOT NULL
+        DEFAULT FALSE,
+    disable_comment VARCHAR(256) NULL
+        DEFAULT NULL,
+
+    CHECK ((disable_comment IS NULL) OR (is_disabled IS TRUE))
 );
 
-CREATE INDEX users_location_idx ON users USING GIST (location);
+CREATE INDEX users_location_idx
+    ON users USING GIST (location);
+
+CREATE TABLE currencies (
+    name VARCHAR(5) PRIMARY KEY
+        CHECK (name ~ '^[A-Z]{3,5}$'),
+    relative_rate NUMERIC(15, 6) NOT NULL
+        CHECK (relative_rate > 0),
+
+    is_disabled BOOLEAN NOT NULL
+        DEFAULT FALSE
+);
 
 CREATE TABLE exchanges (
-    user_telegram_id BIGINT NOT NULL REFERENCES users(telegram_id) ON DELETE CASCADE,
-    currency_give VARCHAR(10) NOT NULL REFERENCES currencies(name) ON DELETE RESTRICT,
-    currency_get VARCHAR(10) NOT NULL REFERENCES currencies(name) ON DELETE RESTRICT,
-    is_dynamic BOOLEAN NOT NULL,
-    rate NUMERIC(20, 6),
-    comment VARCHAR(400),
+    user_telegram_id BIGINT NOT NULL
+        REFERENCES users(telegram_id)
+        ON DELETE RESTRICT,
 
-    PRIMARY KEY (user_telegram_id, currency_give, currency_get)
+    currency_name_give VARCHAR(5) NOT NULL
+        REFERENCES currencies(name)
+        ON DELETE RESTRICT,
+    currency_name_get VARCHAR(5) NOT NULL
+        REFERENCES currencies(name)
+        ON DELETE RESTRICT,
+
+    CHECK (currency_name_give <> currency_name_get),
+    PRIMARY KEY (
+        user_telegram_id,
+        currency_name_give,
+        currency_name_get
+    ),
+
+    is_dynamic BOOLEAN NOT NULL,
+    rate_or_margin NUMERIC(15, 6) NOT NULL,
+
+    comment VARCHAR(512) NULL
+        DEFAULT NULL,
+
+    is_disabled BOOLEAN NOT NULL
+        DEFAULT FALSE,
+    disable_comment VARCHAR(256) NULL
+        DEFAULT NULL,
+
+    CHECK ((disable_comment IS NULL) OR (is_disabled = TRUE))
 );
 
-CREATE INDEX exchanges_currency_pair_idx ON exchanges (currency_give, currency_get);
+CREATE INDEX exchanges_currency_pair_idx
+    ON exchanges(
+        currency_name_give,
+        currency_name_get
+    );
